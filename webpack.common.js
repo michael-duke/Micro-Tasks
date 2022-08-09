240a8c781e1bc449ecc5d17046f5ed88b8e70577
michael-duke <michael.21duke@gmail.com>
2022-08-09 11:01:54 +0300

    Add webpack configs from previous setup and include them in the .eslintignore file

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Test',
+      template: './src/template.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        include: path.join(__dirname, './assets'),
+        loader: 'file-loader',
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
---
+++ webpack.prod.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: 'main.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]',
+    clean: true,
+  },
+});
