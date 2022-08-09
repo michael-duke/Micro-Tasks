6fef2544eec2160c20aa09246581743a8180146f
michael-duke <michael.21duke@gmail.com>
2022-08-09 16:47:13 +0300

    Add the required modules DynamicTodo and CreateTodo

---
+++ src/modules/create-Todo.js
@@ -0,0 +1,10 @@
+// Controller
+class CreateTodo {
+  constructor(id, description, isComplete) {
+    this.id = id;
+    this.description = description;
+    this.isComplete = isComplete;
+  }
+}
+
+export default CreateTodo;
